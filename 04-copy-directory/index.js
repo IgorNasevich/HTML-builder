3ee6115d8d238f18b88f686686b21b935c72900b
IgorNasevich <igor.nasevich007@gmail.com>
2022-05-23 23:59:14 +0300

    feat: done 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,15 @@
+const path = require('path');
+const fs = require('fs');
+
+fs.mkdir(path.join(__dirname, 'files-copy'), {recursive:true}, err => {
+    if (err) throw err;
+    const files = fs.readdir(path.join(__dirname, 'files',), (err, files) => {
+        function callback(err) {
+            if (err) throw err;
+          }
+        for (const file of files){
+            fs.copyFile(path.join(__dirname, "files", file), path.join(__dirname, 'files-copy', file), callback);
+        };
+    });
+});
 
